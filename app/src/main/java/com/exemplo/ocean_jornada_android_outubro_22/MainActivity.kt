e8a686259ad95de8deb6b1d4e0d07652647a71ad
Lila <dalila.analistadesistema@gmail.com>
2022-10-13 10:34:05 -0300

    Segunda tela

--- app/src/main/java/com/exemplo/ocean_jornada_android_outubro_22/MainActivity.kt
+++ app/src/main/java/com/exemplo/ocean_jornada_android_outubro_22/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.exemplo.ocean_jornada_android_outubro_22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,26 +8,26 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        // 1. Detecta clique no botão
-        //   a. Pegar o `Button` pelo `ID`
-        //   b. Criar um `listener` para esse `Button` que foi encontrado
-        // 2. Atualizar o conteúdo o `TextView` para o nosso nome
-        //   a. Pegar o `TextView` pelo `ID`
-        //   b. Atualizar a propriedade `text` do `TextView`
+    // 1. Detecta clique no botão
+    //   a. Pegar o `Button` pelo `ID`
+    //   b. Criar um `listener` para esse `Button` que foi encontrado
+    // 2. Atualizar o conteúdo o `TextView` para o nosso nome
+    //   a. Pegar o `TextView` pelo `ID`
+    //   b. Atualizar a propriedade `text` do `TextView`
 
-        // Variáveis em Kotlin começando com val ou var
-        // Val = não muda o valor depois de inicializada
-        // Var = permite a alteração do valor
-        // Dentro de <>, colocamos o TIPO da informação (ex: Button, TextView, EditText etc.)
-        // Dentro de (), colocamos o ID do elemento (ex: R.id.tvResultado, R.id.btEnviar etc.)
-        val btEnviar = findViewById<Button>(R.id.btEnviar)
-        val tvResultado = findViewById<TextView>(R.id.tvResultado)
-        val etNome = findViewById<EditText>(R.id.etNome)
-        // Para criar um listener para uma View (Button, TextView etc.), utilizamos a
+    // Variáveis em Kotlin começando com val ou var
+    // Val = não muda o valor depois de inicializada
+    // Var = permite a alteração do valor
+    // Dentro de <>, colocamos o TIPO da informação (ex: Button, TextView, EditText etc.)
+    // Dentro de (), colocamos o ID do elemento (ex: R.id.tvResultado, R.id.btEnviar etc.)
+    val btEnviar = findViewById<Button>(R.id.btVoltar)
+    val tvResultado = findViewById<TextView>(R.id.tvResultado)
+    val etNome = findViewById<EditText>(R.id.tvResultado)
+    // Para criar um listener para uma View (Button, TextView etc.), utilizamos a
         // declaração setOnClickListener { }
         btEnviar.setOnClickListener {
             // Dentro das { } (chaves) do Listener, temos o código que é
@@ -37,6 +38,13 @@ class MainActivity : AppCompatActivity() {
             } else {
                 etNome.error = "Digite um nome válido!"
             }
-            }
         }
+
+        val btAbrirNovaTela = findViewById<Button>(R.id.btAbrirNovaTela)
+
+        btAbrirNovaTela.setOnClickListener {
+           val novaTelaIntent =  Intent(this, ResultadoActivity::class.java)
+            startActivity(novaTelaIntent)
+        }
+    }
 }
---
+++ app/src/main/java/com/exemplo/ocean_jornada_android_outubro_22/ResultadoActivity.kt
@@ -0,0 +1,18 @@
+package com.exemplo.ocean_jornada_android_outubro_22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        btVoltar.setOnClickListener {
+            finish()
+       }
+    }
+}
